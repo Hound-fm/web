9f4d7b8cfedd2584378021d029e8ec20f2100a7e
btzr-io <btzr.io@gmail.com>
2021-12-05 00:06:24 -0600

    simplify loader styles

--- src/pages/loading.js
+++ src/pages/loading.js
@@ -4,7 +4,11 @@ export default function LoadingPage() {
   return (
     <Page>
       <div className={"empty-state"}>
-        <div className="loader" />
+        <div class="loading">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </Page>
   );
